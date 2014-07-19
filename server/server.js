06657440e633bfd7ed8a54663efb3d558e0af336
kidshenlong <michaelpm91@googlemail.com>
2014-07-19 17:23:07 +0100

    Removed a lot of unnecessary code

--- server/server.js
+++ server/server.js
@@ -19,29 +19,13 @@ function publish(name, data, except) {
 var server = new WebSocketServer({ port: 8080 });
 
 function loop() {
-    var data = {
-        snakes: { }
-    };
-    var snake;
 
-
-    for(var id in clients) {
-        snake = clients[id].snake;
-        // data
-        data.snakes[id] = snake.positionsArray;
-        /*data.snakes[id] = [
-            //parseFloat(snake.pos[0].toFixed(2)), // x
-            //parseFloat(snake.pos[1].toFixed(2)), // y
-            //parseFloat(snake.pos[2].toFixed(2)),// z
-            snake.positionsArray
-        ];*/
-    }
-    publish('update', data);
 }
 
 setInterval(loop, 1000 / 30 );//Frames
 
 server.on('connection', function(client) {
+    
     console.log('client connected', client.id);
 
 
@@ -60,9 +44,6 @@ server.on('connection', function(client) {
 
     client.on('move', function(data) {
         this.snake.movementDirection = data;
-        //console.log(data);
     });
-
-    publish('create', client.snake.data);
 
 });
--- server/vartest.js
+++
@@ -1,8 +0,0 @@
-var i = 1;
-
-var j = i;
-
-i++;
-
-console.log('var i ' + i);
-console.log('var j ' + j);
